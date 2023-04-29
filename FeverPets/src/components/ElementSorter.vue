d80ae8e48360643a4b61699de97505d6479195e2
Guillermo <guillebeta@gmail.com>
2023-04-29 19:25:32 +0200

    Element sorter component, implemented in PetsList

---
+++ FeverPets/src/components/ElementSorter.vue
@@ -0,0 +1,100 @@
+<script>
+const SORT_ORDERS = {
+  ASC: 'asc',
+  DESC: 'desc'
+}
+
+export default {
+  data() {
+    return {
+      currentSorting: null,
+      sortingOrder: SORT_ORDERS.ASC
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'Sort by: '
+    },
+    paramsToSortBy: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    changeSorting(newSorting) {
+      this.sortingOrder = (this.currentSorting !== newSorting || this.sortingOrder === SORT_ORDERS.DESC) ? SORT_ORDERS.ASC : SORT_ORDERS.DESC
+      this.currentSorting = newSorting
+      this.$emit('changeSorting', { sortBy: this.currentSorting, order: this.sortingOrder })
+    },
+    clearSorting() {
+      this.sortingOrder = SORT_ORDERS.ASC
+      this.currentSorting = null
+      this.$emit('changeSorting', { sortBy: this.currentSorting, order: this.sortingOrder })
+    },
+    getParamClass(param) {
+      let paramClass = ''
+      if (this.currentSorting === param) {
+        paramClass = this.sortingOrder
+      }
+      return paramClass
+    }
+  },
+  computed: {
+    showClearSorting() {
+      return this.currentSorting !== null
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="element-sorter">
+    <span>{{ title }}</span>
+    <div class="sort-params" v-for="param in paramsToSortBy" :key="param">
+      <button :class="getParamClass(param)" @click="changeSorting(param)">{{ param }}</button>
+    </div>
+    <img
+      v-if="showClearSorting"
+      class="clear-sorting"
+      src="src/assets/images/clear_sorting.png"
+      @click="clearSorting"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import 'src/assets/styles/button.scss';
+
+.element-sorter {
+  display: flex;
+  flex-direction: row;
+  min-height: 40px;
+
+  .asc:after {
+    content: ' ▲';
+  }
+
+  .desc:after {
+    content: ' ▼';
+  }
+
+  span {
+    margin-right: 8px;
+  }
+
+  button {
+    min-width: 80px;
+    margin: 0px 8px;
+  }
+
+  .clear-sorting {
+    width: 40px;
+    height: 40px;
+    margin-left: 10px;
+    cursor: pointer;
+    position: relative;
+    bottom: 4px;
+  }
+}
+</style>
--- FeverPets/src/components/PetsList.vue
+++ FeverPets/src/components/PetsList.vue
@@ -1,28 +1,44 @@
 <script>
 import ApiPets from '../services/api-pets.js'
+import ElementSorter from './ElementSorter.vue'
 import PetTile from './PetTile.vue'
 
 const MOCK_LOADING_TIME = 1000
 
 export default {
   components: {
+    ElementSorter,
     PetTile
   },
   data() {
     return {
-      pets: null
+      pets: null,
+      paramsToSortBy: ['weight', 'length', 'height', 'name', 'kind']
     }
   },
   mounted() {
     ApiPets.getAllPets().then(response => {
       setTimeout( () => { this.pets = response.data }, MOCK_LOADING_TIME)
     })
+  },
+  methods: {
+    changeSorting({ sortBy, order }) {
+      ApiPets.getPetsSortedBy(sortBy, order).then(response => {
+        setTimeout( () => { this.pets = response.data }, MOCK_LOADING_TIME)
+      })
+    }
   }
 }
 </script>
 
 <template>
   <div class="pet-list">
+    <ElementSorter
+      v-if="pets"
+      title="Sort pets by:"
+      :paramsToSortBy="paramsToSortBy"
+      @changeSorting="changeSorting"
+    />
     <div v-if="pets" class="pet-carousel">
      <div 
         class="pet-tile-container"
@@ -37,6 +53,10 @@ export default {
 </template>
 
 <style scoped lang="scss">
+.pet-sorter {
+  margin: 16px;
+}
+
 .pet-carousel {
   display: flex;
   flex-flow: row wrap;
